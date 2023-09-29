0763398a5080cdb537d4e0168d23871699d8309a
Madhavi <madhavi.patil1234@gmail.com>
2023-09-29 17:59:23 +0530

    Added search and login page modfied

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,5 +4,9 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("LoginPage");
 	}
+	
+	public void loginSet() {
+		System.out.println("Set");
+	}
 
 }
---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class SearchPage {
+	
+	public void searchTest() {
+		System.out.println("searchTest");
+	}
+
+}
